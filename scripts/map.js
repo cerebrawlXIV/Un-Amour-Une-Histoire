4d9ceaeb9feebe65281c82ce9d40ba6428013b6c
Teddy <cerebrawl@outlook.fr>
2021-11-20 19:23:01 +0100

    [boutique.html] Ajout de la map

---
+++ scripts/map.js
@@ -0,0 +1,7 @@
+let addressIcon = document.querySelector(".address-icon");
+let adressArea = document.querySelector("#address");
+
+addressIcon.addEventListener("click", () => {
+    adressArea.classList.toggle("active");
+    addressIcon.classList.toggle("active")
+});
