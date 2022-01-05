4ad39ca6310206ec1762f9d5eb5531d50fcd6920
Teddy <cerebrawl@outlook.fr>
2022-01-05 16:32:08 +0100

    [fiche-produit] Mise en place des fiches produits prêt-à-porter

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,11 +3,11 @@ const liList = document.querySelector("#desktop-nav .li-list");
 const headerBot = document.querySelector("header .upper-part");
 
 liList.addEventListener("mouseover", () => {
-    navDesktop.classList.toggle("nav-mouse-up");
-    headerBot.classList.toggle("mouse-up");
+    navDesktop.classList.add("nav-mouse-up");
+    headerBot.classList.add("mouse-up");
 });
 
 liList.addEventListener("mouseout", () => {
-    navDesktop.classList.toggle("nav-mouse-up");
-    headerBot.classList.toggle("mouse-up");
+    navDesktop.classList.remove("nav-mouse-up");
+    headerBot.classList.remove("mouse-up");
 });
